cc46cf6b45d8e7035145c19a3e11474e02cd1cbe
Ahmed Tioual <91097298+AhmedTioual@users.noreply.github.com>
2025-01-01 13:35:49 +0000

    Set DJANGO_SETTINGS_MODULE in wsgi.py

--- backend/juridical_chatbot/wsgi.py
+++ backend/juridical_chatbot/wsgi.py
@@ -1,5 +1,12 @@
+import os
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
+# Set the settings module for Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'juridical_chatbot.settings')
+
+# Get the WSGI application
 application = get_wsgi_application()
+
+# Use WhiteNoise to serve static files
 application = WhiteNoise(application)
