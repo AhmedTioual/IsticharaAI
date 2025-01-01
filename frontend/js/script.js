414a95c153bf7d21df18f2c459035c798df0b8ac
Ahmed Tioual <91097298+AhmedTioual@users.noreply.github.com>
2025-01-01 14:05:19 +0000

    Modify JS file

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -124,7 +124,7 @@ async function sendMessage() {
 
     // Call the backend API
     try {
-        const response = await fetch('http://127.0.0.1:8000/api/chat/', {
+        const response = await fetch('https://isticharaai.vercel.app/api/chat/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
