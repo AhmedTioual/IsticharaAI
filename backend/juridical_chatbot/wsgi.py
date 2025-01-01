74ee16e12dce096850c2b1093aa9484c09e47fb9
Ahmed Tioual <91097298+AhmedTioual@users.noreply.github.com>
2025-01-01 14:02:26 +0000

    Wsgi Config

--- backend/juridical_chatbot/wsgi.py
+++ backend/juridical_chatbot/wsgi.py
@@ -3,10 +3,11 @@ from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 # Set the settings module for Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'juridical_chatbot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.juridical_chatbot.settings')
 
-# Get the WSGI application
 application = get_wsgi_application()
 
-# Use WhiteNoise to serve static files
 application = WhiteNoise(application)
+
+# Vercel requires an `app` or `handler` variable to point to the application.
+app = application  # Use `app` instead of `application` for compatibility with Vercel.
