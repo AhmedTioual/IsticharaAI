53931bb24df8ec54a3800938cb6c1725eb7ca56d
Ahmed Tioual <91097298+AhmedTioual@users.noreply.github.com>
2025-01-07 14:06:34 +0100

    Update services.py

--- backend/chatbot/services.py
+++ backend/chatbot/services.py
@@ -4,6 +4,7 @@ import time
 from pinecone import Pinecone
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import re
 
 # get embeddings from LaBSE model
 def get_embeddings(texts):
@@ -107,6 +108,9 @@ def rag_pipeline(user_message):
     chain = prompt | model | parser
 
     result = chain.invoke({'context': context, 'question': user_message})
+    
+    # Remove non-Arabic, non-English, and non-numeric characters
+    response = re.sub(r'[^\u0600-\u06FF\u0750-\u077F\u0041-\u005A\u0061-\u007A\u0030-\u0039\s.,:؛،\n]', '', result)
     
     # Apply formatting to the result string
     result = result.replace("\n", "<br>").replace(":", ":<br>").replace("1.", "<br>1.")
